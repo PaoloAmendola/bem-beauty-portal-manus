0c81b587ca349f668f8d7146703e33cbaae24811
PaoloAmendola <paolo@bindistribuidora.com.br>
2025-07-29 19:34:28 -0300

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Temporarily hardcoding values for debugging environment variable issue
-const supabaseUrl = 'https://avrhqkufesbrbctdujfn.supabase.co';
+const supabaseUrl = 'https://xqbtdowdeumqwtsnruzl.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2cmhxa3VmZXNicmJjdGR1amZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjMzMjksImV4cCI6MjA2NjM5OTMyOX0.xQKT1oEbF8b1FsPwtUWhwYBiEgs8bsWSoNZy-UXr7Bc';
 
 console.log('Supabase URL (hardcoded):', supabaseUrl);
