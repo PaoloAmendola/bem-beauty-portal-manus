bc503c4d1bc8a3f2f1fc09d8d2ec66856b6314f5
PaoloAmendola <paolo@bindistribuidora.com.br>
2025-07-30 18:46:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': '/home/ubuntu/bem-beauty-portal/src',
     },
+  },
+  define: {
+    'process.env.VITE_SUPABASE_URL': JSON.stringify(import.meta.env.VITE_SUPABASE_URL),
+    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(import.meta.env.VITE_SUPABASE_ANON_KEY),
   },
 })
